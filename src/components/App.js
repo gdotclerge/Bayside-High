7a93b80a178d4082274cfbf51e0273ecbbf9cd18
JMoloughney <github email address>
2018-05-23 11:26:44 -0400

    LOL

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react'
 import CourseContainer from './CourseContainer'
 
 class App extends Component {
+
+
+
+
+
   render() {
     return (
       <div className="ui raised segment">
         <div className="ui center aligned segment violet inverted">
           <h2>Bayside High Grade Tracker</h2>
         </div>
-
-        <CourseContainer />
-
+        <CourseContainer/>
       </div>
     )
   }
